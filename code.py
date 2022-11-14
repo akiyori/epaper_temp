8f8fcfc064dae49dee6efbd9ee6600523268e27e
acoustic panther <acousticpanther@gmail>
2022-11-14 22:19:35 +0900

    add plot

--- code.py
+++ code.py
@@ -1,11 +1,13 @@
 import framebuf
 import math
-import time
+import utime
 from epaper75B import EPD_7in5_B
 from thermometer import Thermometer
+from collections import deque
 
 
-def circle(buf: framebuf.FrameBuffer, x, y, r, c):
+def circle(buf: framebuf.FrameBuffer, point, r, c):
+    x, y = point
     buf.hline(x-r, y, r*2, c)
     for i in range(1, r):
         a = int(math.sqrt(r*r-i*i))  # Pythagoras!
@@ -13,61 +15,120 @@ def circle(buf: framebuf.FrameBuffer, x, y, r, c):
         buf.hline(x-a, y-i, a*2, c)  # Upper half
 
 
-def line_w(buf: framebuf.FrameBuffer, x1, y1, x2, y2, w, c):
+def line_w(buf: framebuf.FrameBuffer, point1, point2, w, c):
+    x1, y1 = point1
+    x2, y2 = point2
     for i in range(-1*int(w/2), int(w/2)):
         for j in range(-1*int(w/2), int(w/2)):
             buf.line(x1+i, y1+j, x2+i, y2+j, c)
 
 
-def plot_graph(epd: EPD_7in5_B, data: list[float]):
+class TempGraph:
     cell_width = 80
     cell_height = 80
     row_count = 4
     column_count = 9
+    width = column_count*cell_width
+    height = row_count*cell_height
     margin_top = 130
     margin_left = 40
+    hours_per_cell = 2
+    column_now = -2
+    temp_max = 30
+    temp_per_cell = 5
+    data_count_per_hour = 4
+    data_count_per_cell = hours_per_cell * data_count_per_hour
+    width_per_data = cell_width/data_count_per_cell
+    now_pos = column_count + column_now
+    temp_zero_y = int(margin_top+cell_height * (temp_max/temp_per_cell))
+    height_per_temp = cell_height/temp_per_cell
 
-    # 横線
-    for i in range(0, row_count+1):
-        epd.imageblack.hline(margin_left, margin_top+cell_height*i,
-                             column_count*cell_height, epd.BLACK)
-    # 縦線
-    for i in range(0, column_count+1):
-        epd.imageblack.vline(margin_left+cell_width*i, margin_top,
-                             row_count*cell_width, epd.BLACK)
-    # グラフ
-    last_point = (margin_left, int(round(250-data[0]*4)))
-    for i in range(0, len(data)):
-        point = (margin_left+12*i, int(round(250-data[i]*4)))
-        circle(epd.imagered, point[0], point[1], 5, epd.RED)
-        line_w(epd.imagered, last_point[0], last_point[1],
-               point[0], point[1], 2, epd.RED)
-        last_point = point
-    # Y軸 ラベル
-    for i in range(0, row_count+1):
-        epd.writer_black.text(str(i*10), 8, margin_top+cell_height*i-10)
-        epd.writer_black.text(str(i*10), column_count *
-                              cell_height+48, margin_top+cell_height*i-10)
-    # X軸 ラベル
-    for i in range(0, column_count+1):
-        epd.writer_black.text(str(i*10), margin_left +
-                              cell_width*i-12, margin_top - 30)
+    def __init__(self, epd: EPD_7in5_B):
+        self.epd = epd
+
+    def draw_frame(self):
+        # 横線
+        for i in range(0, self.row_count+1):
+            self.epd.imageblack.hline(
+                self.margin_left, self.margin_top+self.cell_height*i, self.width, epd.BLACK)
+        # 縦線
+        for i in range(0, self.column_count+1):
+            self.epd.imageblack.vline(
+                self.margin_left+self.cell_width*i, self.margin_top, self.height, epd.BLACK)
+        # Y軸 ラベル
+        for i in range(0, self.row_count+1):
+            temp = self.temp_max - i*self.temp_per_cell
+            self.epd.writer_black.text(
+                str(temp), 8, self.margin_top+self.cell_height*i-10)
+            self.epd.writer_black.text(str(temp), self.width+48,
+                                       self.margin_top+self.cell_height*i-10)
+        # X軸 ラベル
+        t = utime.localtime()
+        hour = t[3]
+        for i in range(0, self.column_count+1):
+            value = hour-(self.now_pos-i)*self.hours_per_cell
+            if value < 0:
+                value += 24
+            elif value > 24:
+                value -= 24
+            if value != hour:
+                self.epd.writer_black.text(str(value), self.margin_left +
+                                           self.cell_width*i-12, self.margin_top - 30)
+            else:
+                self.epd.writer_red.text('{:02d}:{:02d}'.format(
+                    t[3], t[4]), self.margin_left +
+                    self.cell_width*i-36, self.margin_top - 30)
+
+    def plot(self, data):
+        print(len(data))
+        self.draw_frame()
+        last_point = (0, 0)
+        last_point_yesterday = (0, 0)
+        for i in range(0, self.column_count*self.data_count_per_cell+1):
+            x = int(self.margin_left+i*self.width_per_data)
+            # current
+            index = self.now_pos*self.data_count_per_cell - i
+            if len(data) > index and i <= self.now_pos*self.data_count_per_cell:
+                temp = data[(len(data)-1)-index]
+                point = (x, self.get_temp_y(temp))
+                circle(epd.imagered, point, 5, epd.RED)
+                if(last_point[0] != 0):
+                    line_w(epd.imagered, last_point, point, 2, epd.RED)
+                last_point = point
+            # yesterday
+            index = index + 24*self.data_count_per_hour
+            if len(data) > index:
+                temp = data[(len(data)-1)-index]
+                point = (x, self.get_temp_y(temp))
+                circle(epd.imageblack, point, 5, epd.BLACK)
+                if(last_point_yesterday[0] != 0):
+                    line_w(epd.imageblack, last_point_yesterday,
+                           point, 2, epd.BLACK)
+                last_point_yesterday = point
+
+    def get_temp_y(self, temp):
+        return int(self.temp_zero_y-(self.height_per_temp*temp))
 
 
 if __name__ == '__main__':
-
+    data = []
     epd = EPD_7in5_B()
-    epd.imageblack.fill(epd.WHITE)
-    epd.imagered.fill(epd.WHITE)
-    t = time.localtime()
-    epd.writer_black.text(str('{:04d}/{:02d}/{:02d} {:02d}:{:02d}:{:02d}'.format(
-        t[0], t[1], t[2], t[3], t[4], t[5])), 5, 10, 40, True)
     thermometer = Thermometer()
-    temp = thermometer.get()
-    epd.writer_black.text(str(temp)+chr(176)+'C', 600, 10, 40, True)
-    plot_graph(epd, [21.5, 22.0, 23.5, 24.0, 27.0, 30.0,
-               28.0, 25.5, 21.5, 19.0, 10.0, 4.5, -1.0, -3.0, 7.5, 12.0, 15.0, 18.0, 20.5,
-               28.0, 25.5, 21.5, 19.0, 10.0, 4.5, -1.0, -3.0, 7.5, 12.0, 15.0, 18.0, 20.5,
-               28.0, 25.5, 21.5, 19.0, 10.0, 4.5, -1.0, -3.0, 7.5, 12.0, 15.0, 18.0, 20.5, ])
-    epd.display()
-    epd.sleep()
+    graph = TempGraph(epd)
+
+    while True:
+        epd.init()
+        temp = thermometer.get()
+        data.append(temp)
+        if(len(data) > 300):
+            data = data[100:]
+        epd.imageblack.fill(epd.WHITE)
+        epd.imagered.fill(epd.WHITE)
+        t = utime.localtime()
+        epd.writer_black.text('{:04d}/{:02d}/{:02d} {:02d}:{:02d}:{:02d}'.format(
+            t[0], t[1], t[2], t[3], t[4], t[5]), 10, 10, 40, True)
+        epd.writer_black.text(str(temp)+chr(176)+'C', 640, 10, 40, True)
+        graph.plot(data)
+        epd.display()
+        epd.sleep()
+        utime.sleep(15*60)
--- epaper75B.py
+++ epaper75B.py
@@ -24,6 +24,18 @@ CS_PIN = 9
 BUSY_PIN = 13
 
 
+def timed_function(f, *args, **kwargs):
+    myname = str(f).split(' ')[1]
+
+    def new_func(*args, **kwargs):
+        t = utime.ticks_us()
+        result = f(*args, **kwargs)
+        delta = utime.ticks_diff(utime.ticks_us(), t)
+        print('Function {} Time = {:6.3f}ms'.format(myname, delta/1000))
+        return result
+    return new_func
+
+
 class EPD_7in5_B:
     WHITE = 0x00
     BLACK = 0xff
